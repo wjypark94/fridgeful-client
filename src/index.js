fb08cafec8e8795835664f96ff1a63c736e1302f
William Park <wjypark94@gmail.com>
2018-05-08 22:54:38 -0700

    Wrapped root component in redux provider

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+     <App />
+  </Provider>,
+   document.getElementById('root')
+   );
 registerServiceWorker();
