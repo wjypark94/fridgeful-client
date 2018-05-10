cb198cc26179e26159e8cb98e28f8c2eda872e2e
William Park <wjypark94@gmail.com>
2018-05-10 01:00:20 -0700

    Fixed typo

--- src/components/App.js
+++ src/components/App.js
@@ -7,22 +7,22 @@ import LoginForm from './login-form';
 
 import Home from './Home';
 import Dashboard from './Dashboard';
-import { connect } from 'react-redux';
+
 
 class App extends Component {
-  
+
   render(){
     return (
       <Router>
-        <div className='app'>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/dashboard' component={Dashboard}/>
-            <Route exact path='/sign-up-form' component={SignUpForm}/>
-            <Route exact path='/login-form' component={LoginForm}/>
+        <div className="app">
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/sign-up-form" component={SignUpForm}/>
+            <Route exact path="/login-form" component={LoginForm}/>
         </div>
       </Router>
     );
   }
 }
 
-export default connect()(App);
+export default App;
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,46 +1,49 @@
-import React, {Component} from 'react';
-import '../componentStyles/Dashboard.css';
+import React, { Component } from 'react';
+
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
+
 import { Link } from 'react-router-dom';
 
-class Dashboard extends Component{
-    constructor(props){
+
+
+class Dashboard extends Component {
+    constructor(props) {
         super(props);
-        this.state ={
+        this.state = {
             "open": false,
         };
     }
 
     handleToggle = () => this.setState({open: !this.state.open});
 
-    render(){
-        return(
-            <div>
-                <div>
-                <AppBar
+
+    render() {
+        return (
+          <div>
+              <div>
+                  <AppBar
                       iconClassNameRight="muidocs-icon-navigation-expand-more"
                       title="Fridgeful"
                       onLeftIconButtonClick={this.handleToggle}
                       className="app-bar"
-                      style={{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', backgroundColor: 'black'}}
+                      style={{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}
                   />
-                 <Drawer
+        
+                  <Drawer
                       docked={false}
                       width={250}
                       open={this.state.open}
                       onRequestChange={(open) => this.setState({open})}>
 
-                      <AppBar title="Fridgeful" style={{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', backgroundColor: 'black'}}/>
-                      <MenuItem  containerElement={<Link to="/" />} id="home">Home</MenuItem>
-                      <MenuItem  id="dashboard">Dashboard</MenuItem>
-                      <MenuItem  id="recipes">Recipes</MenuItem>
-                      <MenuItem  id="logout">Log Out</MenuItem>
+                      <AppBar title="Fridgeful" style={{boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}/>
+                      <MenuItem containerElement={<Link to="/" />} id="home">Home</MenuItem>
+
                   </Drawer>
-                </div>
-            </div>
-        )
+              </div>
+          </div>
+        );
     }
 };
 
