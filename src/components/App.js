fdab2634bdee837cf0153497ffd97faa41b8b04d
William Park <wjypark94@gmail.com>
2018-05-10 00:46:41 -0700

    added connect fixed typo

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../componentStyles/App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-//import Dashboard from './Dashboard';
 
 import SignUpForm from './sign-up-form';
 import LoginForm from './login-form';
 
 import Home from './Home';
 import Dashboard from './Dashboard';
+import { connect } from 'react-redux';
 
-export default function App(props) {
+class App extends Component {
+  
+  render(){
     return (
       <Router>
         <div className='app'>
-          <main>
             <Route exact path='/' component={Home}/>
             <Route exact path='/dashboard' component={Dashboard}/>
             <Route exact path='/sign-up-form' component={SignUpForm}/>
             <Route exact path='/login-form' component={LoginForm}/>
-          </main>
         </div>
       </Router>
     );
+  }
 }
-//<Route exact path='/dashboard' component={Dashboard}/>
 
+export default connect()(App);
