62e1b514f2d2e10a55092b75e5135d1f0a97cf8a
William Park <wjypark94@gmail.com>
2018-05-09 23:54:15 -0700

    fixed app smoke test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {shallow} from 'enzyme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 it ('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(   
+<MuiThemeProvider>
+    <App />
+</MuiThemeProvider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
