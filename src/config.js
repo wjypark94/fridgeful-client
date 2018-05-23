147761b62851b500f5fac5105df64ad6e4361019
William Park <wjypark94@gmail.com>
2018-05-22 19:07:10 -0700

    fixed typos

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://sheltered-headland-53456.herokuapp.com/api',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
 }
