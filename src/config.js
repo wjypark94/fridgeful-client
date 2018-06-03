945e9d690eb49334362cee7a7deb4755088bd952
William Park <wjypark94@gmail.com>
2018-06-02 23:40:35 -0700

    fixed typo

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://serene-citadel-94389.herokuapp.com/api',
 }
