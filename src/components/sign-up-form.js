9cb52ce8f9ea3ff07a8849bf62001d3b7622c203
William Park <wjypark94@gmail.com>
2018-06-02 20:45:14 -0700

    fixed code

--- src/components/sign-up-form.js
+++ src/components/sign-up-form.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import '../componentStyles/sign-up-form.css';
-
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
